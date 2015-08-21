1afe55c88d6cd96ee8f2c40543fc6787ef12c5f8
Andy Duncan <andy@duncaningram.com>
2015-08-21 19:35:01 -0400

    Use polling in rerun for development.

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,7 +15,7 @@ if node.chef_environment == 'development'
   group = 'vagrant'
   app_home = '/vagrant'
   app_path = '/vagrant/lol.rb'
-  rerun = "/home/vagrant/.rbenv/versions/#{node["lol"]["ruby"]["version"]}/bin/rerun "
+  rerun = "/home/vagrant/.rbenv/versions/#{node["lol"]["ruby"]["version"]}/bin/rerun --use-polling "
   ruby = "/home/vagrant/.rbenv/versions/#{node["lol"]["ruby"]["version"]}/bin/ruby"
 else
   include_recipe 'lol::_lol'
